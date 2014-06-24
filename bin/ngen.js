740f8dd3457ae0b9c1aef12b796ed66507309372
Raynos <raynos2@gmail.com>
2014-06-24 14:22:36 -0700

    allow logger

--- bin/ngen.js
+++ bin/ngen.js
@@ -44,7 +44,7 @@ function main(opts, callback) {
     opts.name = opts.name || opts._[0];
     opts.description = opts.description || opts._[1];
 
-    opts.logger = console;
+    opts.logger = opts.logger || console;
 
     // create template
     var tmpl = new Template(opts.template, opts);
