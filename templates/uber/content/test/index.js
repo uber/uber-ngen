64ac6d911016ec876c1893eb317ff02c939a058f
Raynos <raynos2@gmail.com>
2015-02-06 13:40:47 -0800

    move tests into their own file

--- templates/uber/content/test/index.js
+++ templates/uber/content/test/index.js
@@ -1,19 +1,3 @@
 'use strict';
 
-var test = require('tape');
-
-var {{projectName}} = require('../index.js');
-
-test('{{projectName}} is a function', function t(assert) {
-    assert.equal(typeof {{projectName}}, 'function');
-
-    assert.end();
-});
-
-test('{{projectName}} is not implemented', function t(assert) {
-    assert.throws(function throwIt() {
-        {{projectName}}();
-    }, /Not Implemented/);
-
-    assert.end();
-});
+require('./{{project}}.js');
