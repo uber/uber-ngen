f9c313e343abefec3dfd4109d3bc6135580f251b
Raynos <raynos2@gmail.com>
2015-02-05 16:45:42 -0800

    have scaffolded code throw not implemented

--- templates/uber/content/test/index.js
+++ templates/uber/content/test/index.js
@@ -6,5 +6,14 @@ var {{projectName}} = require('../index.js');
 
 test('{{projectName}} is a function', function t(assert) {
     assert.equal(typeof {{projectName}}, 'function');
+
+    assert.end();
+});
+
+test('{{projectName}} is not implemented', function t(assert) {
+    assert.throws(function throwIt() {
+        {{projectName}}();
+    }, /Not Implemented/);
+
     assert.end();
 });
